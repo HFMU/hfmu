8da802b55b7d7b7243308cd0c3f0cd8befd82022
Casper Thule <casper.thule@eng.au.dk>
2018-08-07 08:54:50 +0200

    Removed nix files

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, stdenv }:
-mkDerivation {
-  pname = "hfmu";
-  version = "0.1.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = false;
-  license = stdenv.lib.licenses.bsd3;
-}
